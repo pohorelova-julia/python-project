99de17bffd5ded077abdc7b74fb812706007aabf
pohorelova-julia <119610285+pohorelova-julia@users.noreply.github.com>
2025-05-16 10:34:28 +0300

    Update lesson.py

--- lesson.py
+++ lesson.py
@@ -1,10 +1,12 @@
 from id_generator import IdGenerator
+from lecture import Lecture
+from task import Task
 
 class Lesson:
     def __init__(self, title):
         self.id = IdGenerator.get_next_id("lesson")
         self.title = title
-        self.content = []  # міститиvt Lecture та Task
+        self.content = []  # міститиме об'єкти Lecture та Task
 
     # + лекція/завдання
     def add_content(self, content_item):
@@ -12,13 +14,19 @@ class Lesson:
 
     # отрим лекцій
     def get_lectures(self):
-        from lecture import Lecture
-        return [item for item in self.content if isinstance(item, Lecture)]
+        lectures = []
+        for item in self.content:
+            if isinstance(item, Lecture):
+                lectures.append(item)
+        return lectures
 
     # отрим завдань
     def get_tasks(self):
-        from task import Task
-        return [item for item in self.content if isinstance(item, Task)]
+        tasks = []
+        for item in self.content:
+            if isinstance(item, Task):
+                tasks.append(item)
+        return tasks
 
     def __str__(self):
         return f"Заняття: {self.title} ({len(self.content)} елементів вмісту)"
